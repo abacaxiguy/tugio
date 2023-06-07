481bf703c3b313689aac17c65196c523e9dd6ac6
João Lucas <Jlucasoc@hotmail.com>
2023-06-07 20:02:12 -0300

    fix: 🖼 using tailwind for image size

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     return (
         <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
             <div className="sm:mx-auto sm:w-full sm:max-w-md">
-                <Image alt="Tugio's logo" height={208} width={208} className="mx-auto" src="/images/logo2.svg" />
+                <Image alt="Tugio's logo" height="0" width="0" className="mx-auto w-52 h-auto" src="/images/logo2.svg" />
             </div>
             <AuthForm />
         </div>
