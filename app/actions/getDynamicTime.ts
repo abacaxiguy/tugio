89c6a0a6f2f593ec231baf55cad63e97a4d2ab2e
João Lucas <Jlucasoc@hotmail.com>
2023-06-15 18:16:13 -0300

    feat: ⏰ adding `formatType` to `getDynamicTime`

--- app/actions/getDynamicTime.ts
+++ app/actions/getDynamicTime.ts
@@ -1,8 +1,18 @@
 import { format } from "date-fns";
 
-export default function getDynamicTime(time: Date) {
+export default function getDynamicTime(time: Date, formatType?: string) {
     const locale = Intl.DateTimeFormat().resolvedOptions().locale;
     const tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+    if (!formatType) {
+        const now = new Date();
+        const diff = now.getTime() - time.getTime();
+
+        if (diff < 24 * 60 * 60 * 1000) formatType = "HH:mm";
+        else if (diff < 7 * 24 * 60 * 60 * 1000) formatType = "EEE";
+        else formatType = "dd/MM/yyyy";
+    }
+
     const date = new Date(time).toLocaleString(locale, { timeZone: tz });
-    return format(new Date(date), "HH:mm");
+    return format(new Date(date), formatType);
 }
